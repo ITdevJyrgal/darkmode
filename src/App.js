4d8bf0ca59da18bb5509d8c3d7ac9d21b287d488
ITdevJyrgal <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>
2022-02-17 19:41:22 +0600

    idex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import useLocalStorage from "use-local-storage"
+import "./index.css"
+import "./app.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [theme, setTheme] = useLocalStorage('theme' ? 'dark' : 'light')
+
+    const switchTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme)
+    }
+
+    return (
+        <div className="app" data-theme={theme}>
+            <div className="login">
+                <h1>Login</h1>
+
+                <div className="container">
+                    <div className="top">
+                        <i class="fab fa-google"> </i>
+                        <i class="fab fa-instagram"> </i>
+                        <i class="fab fa-apple"> </i>
+                        <i class="fab fa-youtube"> </i>
+                        <i class="fab fa-linkedin"> </i>
+                    </div>
+                    <p className="divider">
+                        <span> Or</span>
+                    </p>
+                    <form action="">
+                        <label>Email</label>
+                        <input type="email" name="email" placeholder="Enter your email "/>
+                        <label>Password</label>
+                        <input type="password" name="password" placeholder="Enter your password "/>
+                        <div className="remember">
+                            <input type="checkbox" checked="checked"/>
+                            <p>Remember me</p>
+                        </div>
+                        <button>Log In</button>
+                    </form>
+
+
+                    <div className="bottom">
+                        <p>Forgot Password ?</p>
+                        <a href="/"> Reset Password</a>
+                    </div>
+                    <p>Create Account</p>
+                </div>
+
+                <div className="theme-toggle">
+                    <i onClick={switchTheme} class="fas fa-toggle-on"/>
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
